7e4f7a62371694ca82fcbcf11885bfc7ab353f49
mpietrzak <mpietrzak@16c6a306-e5df-0310-b56e-80e2779b13de>
2004-07-25 15:07:38 +0000

    toward valid xhtml

--- trunk/mpy-svn-stats.py
+++ trunk/mpy-svn-stats.py
@@ -62,7 +62,13 @@ def output_stats(config, stats):
     output_dir = config.get_output_dir()
     f = file(output_dir + '/index.html', "w")
     f.write("""
-        <html><head><title>%(title)s</title>
+        <!DOCTYPE html 
+            PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+        <head>
+        <title>%(title)s</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <style type="text/css">
 
             body, td { font-size: 12pt }
